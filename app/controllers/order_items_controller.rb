83f8fe9dca97655e586153a2dde729bf9e72e38e
guilhermezils <guilhermezils@icloud.com>
2021-07-01 21:31:34 -0500

    presentation

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,6 +1,6 @@
 class OrderItemsController < ApplicationController
     def index
-        @order_items = OrderItem.all
+        @order_items = OrderItem.all 
         render json: @order_items
     end
 
